5cad49c65006898a7bcc3b788c7cf5ca19c4ce66
AsHuOvSo <asta@avra.dk>
2020-09-08 11:57:05 +0200

    Initial commit

---
+++ src/ComputeExpression.java
@@ -0,0 +1,9 @@
+public class ComputeExpression {
+    //Starting program
+    public static void main(String[] args) {
+        //Figuring out the answer
+        double answer = (7.5 * 6.5) - (4.5 * 3) / 47.5 - 5.5;
+        //Printing it
+        System.out.print("The result of (7.5 * 6.5) - (4.5 * 3) / 47.5 - 5.5 is " + answer);
+    }
+}
